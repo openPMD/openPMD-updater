433a640ba0280013681bda81436a79fda2c4fd7f
Remi Lehe <remi.lehe@normalesup.org>
2018-11-07 09:55:58 -0800

    Add docstring to trigger automated tests

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,6 +1,12 @@
 """
 This file is part of the openPMD-updater.
 
+It checks that the updater works properly, by applying it to
+an existing example file.
+
+Note that this tests only makes sure that the updater does not crash -
+not that it produces the correct update.
+
 Copyright 2018 openPMD contributors
 Authors: Axel Huebl
 License: ISC
@@ -8,15 +14,14 @@ License: ISC
 import unittest
 from openpmd_updater.Updater import Updater
 
-
 class Test_Updater(unittest.TestCase):
     """
-    ...
+    Class that tests the updater by applying it to an example file
     """
 
     def test_update(self):
         """
-        ...
+        Function that gets called by the tests
         """
         filename = "example_files/1_1_0/structure.h5"
 
@@ -26,4 +31,3 @@ class Test_Updater(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
